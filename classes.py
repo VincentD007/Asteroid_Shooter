910fd5e5ee5568441e88a1c3e5152427f20b3d7f
Vincent D <vincentedward007@gmail.com>
2024-08-24 21:34:39 -0700

    Remade ship.move method to ship.update to include the bullets position aswell. Also added a max bullets on screen limiter.

--- classes.py
+++ classes.py
@@ -1,36 +1,53 @@
 import pygame
 import random
 
-
 class Ship:
-    def __init__(self) -> None:
-        image = pygame.image.load("/home/vincentd007/Desktop/Space_Shooter/yct20hubfk061.webp")
+    def __init__(self, screen) -> None:
+        self.screen = screen
+        image = pygame.image.load("images/fighter_ship1.webp")
         self.image = pygame.transform.scale(image, (200, 200))
         self.rect = pygame.Surface.get_rect(self.image)
-        self.rect.y = 700
+        self.bullets = []
         self.rect.x = 300
+        self.rect.y = 700
 
 
-    def move(self, screen):
+    def update(self):
         pressed_keys = pygame.key.get_pressed()
         if pressed_keys[pygame.K_LEFT]:
             self.rect.x -= 8
         if pressed_keys[pygame.K_RIGHT]:
             self.rect.x += 8
-        screen.blit(self.image, (self.rect.x, self.rect.y))
+        self.screen.blit(self.image, (self.rect.x, self.rect.y))
+        for bullet in self.bullets:
+            if bullet.y - 7 > -30:
+                bullet.y -= 7
+                pygame.draw.rect(self.screen, (255, 255, 255), bullet)
+            elif bullet.y == -28:
+                self.bullets.remove(bullet)
+   
+
+    def shoot(self):
+        new_bullet = pygame.Rect(self.rect.x + 95, self.rect.y, 10, 30)
+        if len(self.bullets) < 6:
+            self.bullets.append(new_bullet)
+
+  
+
 
 
 class AsteroidLevel1:
     def __init__(self) -> None:
-        image = pygame.image.load("/home/vincentd007/Desktop/Space_Shooter/meteor_asteroid_icon_149797.png")
+        image = pygame.image.load("images/asteroid1.png")
         self.asteroid_image = pygame.transform.rotate(pygame.transform.scale(image, (100, 100)), 45)
         self.velocity = 5
+        self.health = 3
         self.rect = pygame.Surface.get_rect(self.asteroid_image)
         self.rect.x = random.randint(0, 700)
         self.y = -100
         
     
     def move(self, screen):
-        self.rect.y += 5
+        self.rect.y += self.velocity
         screen.blit(self.asteroid_image, (self.rect.x, self.rect.y))
         pass
--- main.py
+++ main.py
@@ -1,31 +1,37 @@
 import pygame
 from classes import *
 
-
 pygame.init()
 HEIGHT = 900
 WIDTH = 800
-bg_img = pygame.image.load("/home/vincentd007/Desktop/Space_Shooter/—Pngtree—space game earth star background_1592602.png")
+bg_img = pygame.image.load("images/Space_Background.png")
 background = pygame.transform.scale(bg_img, (WIDTH, HEIGHT))
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Asteroid Shooter")
 clock = pygame.time.Clock()
 asteroid = AsteroidLevel1()
-ship = Ship()
+ship = Ship(SCREEN)
 
-def main():
+def main( ):
     play_game = True
     while play_game:
         clock.tick(60)
+        fire_bullet = False
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 play_game = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    fire_bullet = True
+
         SCREEN.fill((0, 0, 0))
         SCREEN.blit(background, (0, 0))
-        ship.move(SCREEN)
-        asteroid.move(SCREEN)
+        if fire_bullet:
+            ship.shoot()
+        ship.update()
+        asteroid.move(SCREEN) 
         pygame.display.update()
-    pygame.quit()
+    pygame.quit()  
 
 
 
