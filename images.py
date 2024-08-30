ad159f3180fec742f8011586f46f01182feb7d69
Vincent D <vincentedward007@gmail.com>
2024-08-30 12:08:26 -0700

    lvl4 asteroid added

--- images.py
+++ images.py
@@ -9,14 +9,15 @@ asteroid_health_image = [asteroid_h1, asteroid_h2, asteroid_h3, asteroid_h4]
 asteroid_image_lvl1 = pygame.transform.rotate(pygame.transform.scale(pygame.image.load("images/asteroid1.png"), (80, 80)), 45)
 asteroid_image_lvl2 = pygame.transform.rotate(pygame.transform.scale(pygame.image.load("images/asteroid2.png"), (80, 80)), 55)
 asteroid_image_lvl3 = pygame.transform.rotate(pygame.transform.scale(pygame.image.load("images/asteroid3.png"), (80, 80)), 60)
-asteroid_images = [asteroid_image_lvl1, asteroid_image_lvl2, asteroid_image_lvl3]
+asteroid_image_lvl4 = pygame.transform.rotate(pygame.transform.scale(pygame.image.load("images/asteroid4.png"), (80, 80)), 60)
+
+asteroid_images = [asteroid_image_lvl1, asteroid_image_lvl2, asteroid_image_lvl3, asteroid_image_lvl4]
 
 level_text_font = pygame.font.SysFont("Comic Sans", 150, False, False)
-level1_text = level_text_font.render("Level 1", 1, (255, 255, 255))
-level2_text = level_text_font.render("Level 2", 1, (255, 255, 255))
-level3_text = level_text_font.render("Level 3", 1, (255, 255, 255))
-level_text = [level1_text, level2_text, level3_text]
 
 heart_image = pygame.transform.scale(pygame.image.load("images/heart.png"), (25, 25))
 
 level_text_font = pygame.font.SysFont("Comic Sans", 150, False, False)
+
+game_loose = level_text_font.render("Game Over", 1, (255, 50, 0))
+game_win = level_text_font.render("You Win!", 1, (0, 255, 0))
